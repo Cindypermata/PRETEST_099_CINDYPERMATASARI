33e69e33644aaaa76331259ddac32fea63e936f5
Cindypermata <113223444+Cindypermata@users.noreply.github.com>
2023-10-20 08:11:57 +0700

    Add files via upload

---
+++ PRETEST_099/src/pretest_099/PRETEST_099.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pretest_099;
+
+/**
+ *
+ * @author asus
+ */
+public class PRETEST_099 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
